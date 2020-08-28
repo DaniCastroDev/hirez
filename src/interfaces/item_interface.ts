9e27da377279dfe1f06993ebc0436588a950bb1e
JoShMiQueL <joshmiquel.cs@gmail.com>
2020-08-28 18:02:14 +0200

    Update

--- src/interfaces/item_interface.ts
+++ src/interfaces/item_interface.ts
@@ -1,19 +1,19 @@
 export default interface Item {
-    ActiveFlag:      string;
-    ChildItemId:     number;
-    DeviceName:      string;
-    IconId:          number;
-    ItemDescription: ItemDescription;
-    ItemId:          number;
-    ItemTier:        number;
-    Price:           number;
-    RestrictedRoles: string;
-    RootItemId:      number;
-    ShortDesc:       string;
-    StartingItem:    boolean;
-    Type:            string;
-    itemIcon_URL:    string;
-    ret_msg:         string | null;
+    ActiveFlag?:      string;
+    ChildItemId?:     number;
+    DeviceName?:      string;
+    IconId?:          number;
+    ItemDescription?: ItemDescription;
+    ItemId?:          number;
+    ItemTier?:        number;
+    Price?:           number;
+    RestrictedRoles?: string;
+    RootItemId?:      number;
+    ShortDesc?:       string;
+    StartingItem?:    boolean;
+    Type?:            string;
+    itemIcon_URL?:    string;
+    ret_msg?:         string | null;
 }
 
 interface ItemDescription {
